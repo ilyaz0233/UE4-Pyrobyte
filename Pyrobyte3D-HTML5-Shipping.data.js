a399f432ab810b0efc80af16c352a7efebd50f0c
ilyaz0233 <119028998+ilyaz0233@users.noreply.github.com>
2022-11-25 13:20:13 +0700

    Update Pyrobyte3D-HTML5-Shipping.data.js

--- Pyrobyte3D-HTML5-Shipping.data.js
+++ Pyrobyte3D-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'D:/3d portfolio/Pyrobyte3D/Binaries/HTML5/Pyrobyte3D-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'Pyrobyte3D-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/p2zldiczr5h5okf/Pyrobyte3D-HTML5-Shipping.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/p2zldiczr5h5okf/Pyrobyte3D-HTML5-Shipping.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
