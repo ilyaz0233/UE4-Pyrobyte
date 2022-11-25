6742d98eec44628f65a6443b74b650375b93de46
ilyaz0233 <119028998+ilyaz0233@users.noreply.github.com>
2022-11-25 13:29:03 +0700

    Update Pyrobyte3D-HTML5-Shipping.UE4.js

--- Pyrobyte3D-HTML5-Shipping.UE4.js
+++ Pyrobyte3D-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Pyrobyte3D-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/ilyaz0233/UE4-Pyrobyte/5d81779669f204675d6e6cb8d23407f736b4c3f9/Pyrobyte3D-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
